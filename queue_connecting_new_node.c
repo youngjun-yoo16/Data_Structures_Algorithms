ff57bc886c301d3745dd489086bd5c4384fc894c
Youngjun Yoo <yoo156@purdue.edu>
2020-07-22 05:01:57 +0900

    Create queue_connecting_new_node.c

---
+++ queue_connecting_new_node.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	char data;
+	struct node* next;
+};
+typedef struct node NODE, * pNODE;
+
+struct queue {
+	pNODE rear;
+	pNODE front;
+};
+typedef struct queue Q;
+
+// 새로 동적할당으로 노드를 만들고
+// 노드의 값을 초기화 하는 함수
+pNODE get_node(char data);
+int enQ(Q* queue, char data);
+char deQ(Q* queue);
+
+int main()
+{
+	// pNODE front = NULL;
+	// pNODE rear = NULL;
+	Q q = { .front = NULL, .rear = NULL };
+	enQ(&q, 'A');
+	enQ(&q, 'B');
+	enQ(&q, 'C');
+	enQ(&q, 'D');
+
+	// front는 가장 앞에 있는 A노드의 주소
+	// rear은 가장 마지막 노드 D의 주소
+	// D->next는 NULL
+	for (pNODE i = q.front; i != NULL; i = i->next)
+	{
+		printf("%c:", i->data);
+	}
+	printf("\n");
+	// A:B:C:D:
+
+	while (q.front != NULL)
+	{
+		printf("%c:", deQ(&q));
+	}
+	printf("\n");
+
+	return 0;
+}
+
+char deQ(Q* queue)
+{
+	// front가 가르키는 노드의 데이터 임시 저장 (반환용)
+	char data = queue->front->data;
+
+	// front의 다음 노드 임시 저장 (free 용)
+	pNODE next = queue->front->next;
+
+	// front가 가르키는 노드 free		
+	free(queue->front);
+
+	// front를 front의 다음노드로 변경
+	queue->front = next;
+
+	// 데이터 반환
+	return data;
+}
+
+int enQ(Q* queue, char data) {
+	// 새로운 노드를 만듦
+	// 만들어진 노드 초기화
+	pNODE new = get_node(data);
+
+	// 만약 현재 노드가 비어있다면 
+			// front가 새로운 노드를 가르키도록
+	// 아니라면 (큐가 있다면)
+			// 기존 queue와 새로 만든 노드를 연결(rear와 연결)
+	if (queue->front == NULL)
+	{
+		queue->front = new;
+	}
+	else
+	{
+		queue->rear->next = new;
+	}
+
+	// rear가 새로운 노드를 가르키도록 변경
+	queue->rear = new;
+
+	// 성공시 0 반환
+	return 0;
+}
+
+// 새로 동적할당으로 노드를 만들고 노드의 값을 초기화하는 함수
+pNODE get_node(char data)
+{
+	pNODE new = malloc(sizeof(NODE));
+	new->data = data;
+	new->next = NULL;
+	return new;
+}
